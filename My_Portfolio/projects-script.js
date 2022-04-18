be8d9e3a394b5dc53d1acbdbf0062533068f734c
Stephen <stephen.lyssy@gmail.com>
2022-04-17 20:28:22 -0500

    Added another project to my projects page.

--- My_Portfolio/projects-script.js
+++ My_Portfolio/projects-script.js
@@ -3,11 +3,7 @@ const navbarLinks = document.querySelector('.navbar-links');
 const projectLinks = document.querySelector('.projects-navlinks');
 const mainJobCostingImage = document.querySelector('#job-costing-image');
 const mainFloodImage = document.querySelector('#flood-image');
-const slides = document.querySelectorAll('.slide');
-const slider = document.querySelector('.slider');
-const btnLeft = document.querySelector('.slider__btn--left');
-const btnRight = document.querySelector('.slider__btn--right');
-const dotContainer = document.querySelector('.dots');
+const mainEbirdsImage = document.querySelector('#ebirds-image');
 
 //%Navbar toggle button event listener
 toggleButton.addEventListener('click', (e) => {
@@ -34,4 +30,9 @@ function selectImageJobCosting(num) {
 function selectImageFlood(num) {
   const thumb = `images/project-images/carousel${num}.png`;
   mainFloodImage.src = thumb;
+}
+
+function selectImageEbirds(num) {
+  const thumb = `images/project-images/carousel${num}.png`;
+  mainEbirdsImage.src = thumb;
 }
